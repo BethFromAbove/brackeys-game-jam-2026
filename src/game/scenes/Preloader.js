af99f3c2faa7f156652fa4d044c0c60757dbc2a2
Beth <bethcott@hotmail.co.uk>
2026-08-30 00:44:16 +0100

    Finished interactive how to play scene

--- src/game/scenes/Preloader.js
+++ src/game/scenes/Preloader.js
@@ -11,12 +11,13 @@ export class Preloader extends Scene
     {
         //  We loaded this image in our Boot Scene, so we can display it here
         this.add.image(400, 300, 'background');
+        this.add.image(400, 200, 'logo');
 
         //  A simple progress bar. This is the outline of the bar.
-        this.add.rectangle(400, 300, 468, 32).setStrokeStyle(1, 0xffffff);
+        this.add.rectangle(400, 400, 468, 32).setStrokeStyle(1, 0xffffff);
 
         //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        const bar = this.add.rectangle(400-230, 300, 4, 28, 0xffffff);
+        const bar = this.add.rectangle(400-230, 400, 4, 28, 0xffffff);
 
         //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
         this.load.on('progress', (progress) => {
