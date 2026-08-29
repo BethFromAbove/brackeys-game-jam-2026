a941e36c2509f334136531032dd61615533e4b6d
Kimbsy <lordkimber@gmail.com>
2026-08-29 23:46:18 +0100

    remove interaction block

--- src/game/scenes/Boot.js
+++ src/game/scenes/Boot.js
@@ -17,15 +17,17 @@ export class Boot extends Scene
 
     create ()
     {
-        this.cameras.main.setBackgroundColor(0xa1a1a1);
+        // this.cameras.main.setBackgroundColor(0xa1a1a1);
 
-        this.add.text(400, 300, '< click here >', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#000000',
-            align: 'center'
-        }).setOrigin(0.5);
+        // this.add.text(400, 300, '< click here >', {
+        //     fontFamily: 'Arial Black', fontSize: 38, color: '#000000',
+        //     align: 'center'
+        // }).setOrigin(0.5);
 
-        this.input.once('pointerdown', () => {
-            this.scene.start('Preloader');
-        });
+        // this.input.once('pointerdown', () => {
+        //     this.scene.start('Preloader');
+        // });
+
+        this.scene.start('Preloader');
     }
 }
